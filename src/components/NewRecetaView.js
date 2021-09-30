031db07b0b0cd91b7584aef3925f06971ad7adff
Itzel Ventura <venit1123@gmail.com>
2021-09-29 19:57:42 -0700

    Install react router

---
+++ src/components/NewRecetaView.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class NewRecetaView extends Component {
+    render() {
+        return (
+            <h1>NEW RECETA FORM WILL BE HERE</h1>
+        )
+    }
+}
+export default NewRecetaView
